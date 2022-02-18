70e7d1986d349ab2b93518ff336441c37514a813
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-02-18 20:44:56 +0100

    klasa ProductViewSet - cała + urle

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,21 +1,38 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from products.models import Product
+from products.serializers import ProductSerializer
 
 
 class ProductViewSet(viewsets.ViewSet):
     def list(self, request):
-        pass
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request):
-        pass
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
-        pass
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def update(self, request, pk=None):
-        pass
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(instance=product, date=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def delete(self, request, pk=None):
-        pass
+        product = Product.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
